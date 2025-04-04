e42f0a0c623b029751f4b62deff6ff8c2f784bce
Ren <roj23813@uvg.edu.gt>
2025-04-03 19:30:07 -0600

    Primero inicios de BD/backend (me enteré el front va en repo aparte)

---
+++ backend/go.mod
@@ -0,0 +1,3 @@
+module ejLogin
+
+go 1.22.2
---
+++ backend/handlers/models/dtos.go
@@ -0,0 +1,13 @@
+package models
+
+type LoginRequest struct {
+Username string `json: "username"`
+Password string `json "password"`
+}
+
+type RegisterRequest struct {
+	Username string `json:"username"`
+	Password string `json:"password"`
+}
+
+
